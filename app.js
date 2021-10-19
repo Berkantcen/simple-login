4ee79fc3848ca960ee23cfce714d66d11da27c2b
Berkant Cen <berkantyazilim@gmail.com>
2021-10-19 09:33:43 +0300

    add return button

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 const userDatabase = [
   {
-    username: "berkant",
-    password: "123456",
+    username: 'berkant',
+    password: '123456',
   },
   {
-    username: "ipek",
-    password: "123456",
+    username: 'ipek',
+    password: '123456',
   },
   {
-    username: "tolga",
-    password: "123456",
+    username: 'tolga',
+    password: '123456',
   },
   {
-    username: "cemal",
-    password: "123456",
+    username: 'cemal',
+    password: '123456',
   },
 ];
 
 // DOM ELEMENTS
-const loginForm = document.querySelector(".login-form");
-const loginBtn = document.querySelector(".login-btn");
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
-const errorMessage = document.querySelector(".message");
-const loginSuccess = document.querySelector(".login-true");
+const loginForm = document.querySelector('.login-form');
+const loginBtn = document.querySelector('.login-btn');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const errorMessage = document.querySelector('.message');
+const loginSuccess = document.querySelector('.login-true');
 
-loginBtn.addEventListener("click", function (event) {
+loginBtn.addEventListener('click', function (event) {
   let username = usernameInput.value;
   let password = passwordInput.value;
 
   // Empty field handling
-  if (username === "" || password === "") {
-    errorMessage.classList.add("active");
+  if (username === '' || password === '') {
+    errorMessage.classList.add('active');
   }
   //   Other outcomes
   loginProcess(username, password);
@@ -54,14 +54,24 @@ function isUserValid(username, password) {
 
 function loginProcess(username, password) {
   if (isUserValid(username, password)) {
-    loginForm.classList.remove("active");
+    loginForm.classList.remove('active');
     loginSuccess.innerHTML = `
-        <h1>Welcome ${username}</h1>
-        <h2>Succesfully Login In</h2>
+        <h1>Welcome <span class="text-primary">${username} </span></h1>
+        <h2>Succesfull Login </h2>
         <p>Enjoy the content of our site</p>
+        <a href="" class="return-start">Return</a>
     `;
-    loginSuccess.classList.add("active");
+    loginSuccess.classList.add('active');
   } else {
-    errorMessage.classList.add("active");
+    errorMessage.classList.add('active');
   }
 }
+
+// Return Starting State
+
+const returnStart = document.querySelector('.return-start');
+
+returnStart.addEventListener('click', function (e) {
+  loginSuccess.classList.remove('active');
+  loginForm.classList.add('active');
+});
