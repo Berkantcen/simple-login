5db81cf6e0abaf1e2e712d083f9b3d129b899ebc
Berkant CEN <69425673+Berkantcen@users.noreply.github.com>
2021-10-19 08:10:03 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const userDatabase = [
+  {
+    username: "berkant",
+    password: "123456",
+  },
+  {
+    username: "ipek",
+    password: "123456",
+  },
+  {
+    username: "tolga",
+    password: "123456",
+  },
+  {
+    username: "cemal",
+    password: "123456",
+  },
+];
+
+// DOM ELEMENTS
+const loginForm = document.querySelector(".login-form");
+const loginBtn = document.querySelector(".login-btn");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const errorMessage = document.querySelector(".message");
+const loginSuccess = document.querySelector(".login-true");
+
+loginBtn.addEventListener("click", function (event) {
+  let username = usernameInput.value;
+  let password = passwordInput.value;
+
+  // Empty field handling
+  if (username === "" || password === "") {
+    errorMessage.classList.add("active");
+  }
+  //   Other outcomes
+  loginProcess(username, password);
+  event.preventDefault();
+});
+
+//  FUNCTION
+
+function isUserValid(username, password) {
+  for (let i = 0; i < userDatabase.length; i++) {
+    if (
+      username === userDatabase[i].username &&
+      password === userDatabase[i].password
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function loginProcess(username, password) {
+  if (isUserValid(username, password)) {
+    loginForm.classList.remove("active");
+    loginSuccess.innerHTML = `
+        <h1>Welcome ${username}</h1>
+        <h2>Succesfully Login In</h2>
+        <p>Enjoy the content of our site</p>
+    `;
+    loginSuccess.classList.add("active");
+  } else {
+    errorMessage.classList.add("active");
+  }
+}
